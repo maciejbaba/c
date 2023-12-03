853b79cb246c7e785b37c165e8d3c04731eb9813
Maciej Bąba <maciek2baba2@gmail.com>
2023-12-04 00:20:23 +0100

    fun with fib

--- fib.c
+++ fib.c
@@ -12,11 +12,11 @@ int fib_rec(int n)
   }
 }
 
-int fib_iter(int n)
+unsigned long long fib_iter(int n)
 {
-  int first = 1;
-  int second = 1;
-  int result = 0;
+  unsigned long long first = 1;
+  unsigned long long second = 1;
+  unsigned long long result = 0;
 
   if (n == 0 || n == 1)
   {
@@ -65,6 +65,13 @@ int main()
   result = fib_iter(n);
   printf("The %dth fibonacci number is: %d\n", n, result);
   printf("The %dth fibonacci number should be: %d\n", n, 832040);
+
+  // 93 is the largest fibonacci number that can be stored in a 64-bit integer
+  // it takes less than a second to calculate !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  n = 93;
+  unsigned long long int long_res = fib_iter(n);
+  printf("The %dth fibonacci number is: %llu\n", n, long_res);
+  printf("The %dth fibonacci number should be: %llu\n", n, 12200160415121876738);
 
   return 0;
 }
